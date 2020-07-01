9f6854d559d52aaacb194261fe40ead1b29e06fb
hpc <philipchanhk626@gmail.com>
2020-07-01 10:42:56 -0400

    dynamic loading of html pages

--- server.py
+++ server.py
@@ -5,22 +5,6 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-  return render_template('contact.html')
-
-@app.route('/work')
-def work():
-  return render_template('work.html')
-
-@app.route('/works')
-def works():
-  return render_template('works.html')
-
-@app.route('/components')
-def components():
-  return render_template('components.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+  return render_template(page_name + '.html')
