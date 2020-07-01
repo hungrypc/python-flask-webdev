d2a4f66efa04f34c91299d58c91185071a7ae133
hpc <philipchanhk626@gmail.com>
2020-07-01 10:56:29 -0400

    moved favicon, form data can be submitted

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,12 @@ def home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
   return render_template(page_name + '.html')
+
+@app.route('/submit_form', method=['POST', 'GET'])
+def submit_form():
+  if request.method == 'POST':
+    data = request.form.to_dict()
+    print(data)
+    return 'form submitted'
+  else:
+    return 'something went wrong'
