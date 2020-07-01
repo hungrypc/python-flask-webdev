8d73216559f8f751f8370eba34b574126a418b47
hpc <philipchanhk626@gmail.com>
2020-07-01 10:29:27 -0400

    portfolio set up

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-  return render_template('index.html', name=username, post_id=post_id)
+@app.route('/')
+def home():
+  return render_template('index.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+@app.route('/work')
+def work():
+  return render_template('work.html')
+
+@app.route('/works')
+def works():
+  return render_template('works.html')
+
+@app.route('/components')
+def components():
+  return render_template('components.html')
