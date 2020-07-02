e9971ad3d28ee586ef1c81166e0a80340beacdb2
hpc <philipchanhk626@gmail.com>
2020-07-02 09:59:38 -0400

    form submit page, contact able to save message

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,11 +9,19 @@ def home():
 def html_page(page_name):
   return render_template(page_name + '.html')
 
-@app.route('/submit_form', method=['POST', 'GET'])
+def write_to_file(data):
+  with open('db.txt', mode='a') as database:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    file = database.write("\n" + email + "\n" + subject + "\n" + message + "\n")
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
   if request.method == 'POST':
     data = request.form.to_dict()
-    print(data)
-    return 'form submitted'
+    write_to_file(data)
+    return redirect('/thanks')
   else:
     return 'something went wrong'
